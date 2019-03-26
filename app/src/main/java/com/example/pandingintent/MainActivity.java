b5171220b7efb6f59c085cc7f151ef65a57828c2
yulia_norkina <yulia.norkina2@gmail.com>
2019-03-26 12:30:47 +0200

    create UI

--- app/src/main/java/com/example/pandingintent/MainActivity.java
+++ app/src/main/java/com/example/pandingintent/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.pandingintent;
 
+import android.app.PendingIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNumber = findViewById(R.id.etNumber);
+        findViewById(R.id.bCalculate).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
     }
 }
